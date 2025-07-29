deb4d7f0869521a3b47bdc6fa062b7a73e874dc3
trantuananh-17 <anhkyohauik17@gmail.com>
2025-07-29 08:10:21 +0700

    refactor/service-structure

---
+++ Backend/src/modules/candidate-profile/service/implements/candidate-profile.service.impl.ts
@@ -0,0 +1,66 @@
+import { CandidateProfile } from '@prisma/client';
+import { BadRequestException, NotFoundException } from '~/global/core/error.core';
+import { ICandidateProfile } from '../../interfaces/candidate-profile.interface';
+import { candidateProfileRepository } from '../../repositories/implements/candidate-profile.repository.impl';
+import { ICandidateProfileRepository } from '../../repositories/candidate-profile.repository';
+import { ICandidateProfileService } from '../candidate-profile.service';
+
+class CandidateProfileService implements ICandidateProfileService {
+  public async create(requestBody: ICandidateProfile, userId: number): Promise<CandidateProfile> {
+    const existingProfile = await candidateProfileRepository.getExistProfile(userId);
+
+    if (existingProfile) {
+      throw new BadRequestException('Hồ sơ đã tồn tại, không thể tạo mới');
+    }
+
+    const candidateProfile = await candidateProfileRepository.createCandidateProfile(requestBody, userId);
+
+    return candidateProfile;
+  }
+
+  public async getAll(): Promise<CandidateProfile[] | []> {
+    const candidateProfiles: CandidateProfile[] = await candidateProfileRepository.findAll();
+
+    return candidateProfiles;
+  }
+
+  public async getOne(id: number): Promise<CandidateProfile> {
+    const candidateProfile: CandidateProfile | null = await candidateProfileRepository.findById(id);
+
+    if (!candidateProfile) {
+      throw new NotFoundException('Không tìm thấy thông tin hồ sơ ứng viên');
+    }
+
+    return candidateProfile;
+  }
+
+  public async update(id: number, requestBody: Partial<ICandidateProfile>): Promise<CandidateProfile> {
+    await this.getOne(id);
+
+    // Lọc trường nào thiếu
+    const data = Object.fromEntries(
+      Object.entries({
+        ...requestBody,
+        dateofbirth: requestBody.dateofbirth ? new Date(requestBody.dateofbirth) : undefined
+      }).filter(([_, v]) => v !== undefined)
+    );
+
+    const profileUpdated: CandidateProfile = await candidateProfileRepository.update(id, data);
+
+    return profileUpdated;
+  }
+
+  public async changeOpenToWorkStatus(id: number): Promise<void> {
+    const profile = await this.getOne(id);
+
+    await candidateProfileRepository.updateOpenToWorkStatus(id, profile.openToWork);
+  }
+
+  public async delete(id: number): Promise<void> {
+    await this.getOne(id);
+
+    await candidateProfileRepository.delete(id);
+  }
+}
+
+export const candidateProfileService: ICandidateProfileService = new CandidateProfileService();
--- Backend/src/modules/user/services/auth.service.ts
+++ Backend/src/modules/user/services/auth.service.ts
@@ -1,58 +1,7 @@
-import bcrypt from 'bcrypt';
-import 'dotenv/config';
-import { ConflictException, UnauthorizedException } from '~/global/core/error.core';
-import prisma from '~/prisma';
-import { generateAccessToken, generateRefreshToken } from '~/global/helpers/token.helper';
-import { IUser, IUserSignUp } from '../interfaces/user.interface';
 import { ITokenResponse } from '../interfaces/auth.interface';
-import { userRepository } from '../Repository/implements/user.repository.impl';
+import { IUser, IUserSignUp } from '../interfaces/user.interface';
 
-class AuthService {
-  public async signUp(requestBody: IUserSignUp): Promise<ITokenResponse> {
-    const { name, email, password } = requestBody;
-
-    const hash = await bcrypt.hash(password, 10);
-
-    const userByEmail = await userRepository.findUserByEmail(email);
-
-    if (userByEmail) {
-      throw new ConflictException('Email đã được đăng ký');
-    }
-
-    const user = await prisma.user.create({
-      data: {
-        name: name,
-        email: email,
-        password: hash
-      }
-    });
-
-    const accessToken = generateAccessToken(user);
-    const refressToken = generateRefreshToken(user);
-
-    return { access_token: accessToken, refresh_token: refressToken };
-  }
-
-  public async signIn(requestBody: IUser): Promise<ITokenResponse> {
-    const { email, password } = requestBody;
-
-    const userByEmail = await userRepository.findUserByEmail(email);
-
-    if (!userByEmail) {
-      throw new UnauthorizedException('Thông tin đăng nhập không chính xác');
-    }
-
-    const isMatchPassword = await bcrypt.compare(password, userByEmail.password);
-
-    if (!isMatchPassword) {
-      throw new UnauthorizedException('Thông tin đăng nhập không chính xác');
-    }
-
-    const accessToken = generateAccessToken(userByEmail);
-    const refressToken = generateRefreshToken(userByEmail);
-
-    return { access_token: accessToken, refresh_token: refressToken };
-  }
+export interface IAuthService {
+  signUp(requestBody: IUserSignUp): Promise<ITokenResponse>;
+  signIn(requestBody: IUser): Promise<ITokenResponse>;
 }
-
-export const authService: AuthService = new AuthService();
