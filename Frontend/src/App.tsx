5eba9126bbd1b82231089a764df3f809ff6cf0e8
trantuananh-17 <anhkyohauik17@gmail.com>
2025-08-01 00:34:19 +0700

    init project frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <h2 className="text-2xl text-blue-500">Hello world</h2>;
+}
+
+export default App;
