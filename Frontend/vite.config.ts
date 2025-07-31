6938153bc819efce4dfd996841126e3b1fb26e0e
trantuananh-17 <anhkyohauik17@gmail.com>
2025-08-01 00:56:28 +0700

    add rule prettier and eslint config

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src')
+    }
+  }
 });
