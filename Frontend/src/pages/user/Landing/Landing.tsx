c5fbfd75079b59ae808d021a74870d9f45bb00a4
trantuananh-17 <anhkyohauik17@gmail.com>
2025-08-01 21:44:07 +0700

    add banner and carousel ui

--- Frontend/src/pages/user/Landing/Landing.tsx
+++ Frontend/src/pages/user/Landing/Landing.tsx
@@ -1,5 +1,13 @@
+import Banner from './components/Banner';
+import Carousel from './components/Carousel';
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div>
+      <Banner />
+      <Carousel />
+    </div>
+  );
 };
 
 export default Landing;
