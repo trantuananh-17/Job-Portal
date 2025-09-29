42c1358c76eedd634075847b14172096670b95a3
trantuananh-17 <anhkyohauik17@gmail.com>
2025-09-29 11:23:33 +0700

    refactor: di order

--- Backend/src/modules/order/repositories/implements/order.repository.impl.ts
+++ Backend/src/modules/order/repositories/implements/order.repository.impl.ts
@@ -1,33 +1,33 @@
-import { Order, OrderStatus } from '@prisma/client';
+import { Order, OrderStatus, PrismaClient } from '@prisma/client';
 import { BaseRepository } from '~/global/base/repositories/implements/base.repository.impl';
 import { IOrderRepository } from '../order.repository';
 import prisma from '~/prisma';
 
 class OrderRepository extends BaseRepository<Order> implements IOrderRepository {
-  constructor() {
+  constructor(private readonly prisma: PrismaClient) {
     super(prisma.order);
   }
 
   async getMyOrder(userId: number): Promise<Order[]> {
-    return await prisma.order.findMany({
+    return await this.prisma.order.findMany({
       where: { recruiterId: userId }
     });
   }
 
   async findFirstByUser(id: number, userId: number): Promise<Order | null> {
-    return await prisma.order.findFirst({
+    return await this.prisma.order.findFirst({
       where: { id, recruiterId: userId }
     });
   }
 
   async findFirstByAdmin(id: number): Promise<Order | null> {
-    return await prisma.order.findFirst({
+    return await this.prisma.order.findFirst({
       where: { id }
     });
   }
 
   async createOrder(packageId: number, recruiterId: number, totalPrice: number): Promise<Order> {
-    return await prisma.order.create({
+    return await this.prisma.order.create({
       data: {
         packageId,
         recruiterId,
@@ -37,7 +37,7 @@ class OrderRepository extends BaseRepository<Order> implements IOrderRepository 
   }
 
   async updateStatusOrder(id: number, status: OrderStatus): Promise<Order> {
-    return await prisma.order.update({
+    return await this.prisma.order.update({
       where: { id },
       data: {
         status
@@ -46,4 +46,4 @@ class OrderRepository extends BaseRepository<Order> implements IOrderRepository 
   }
 }
 
-export const orderRepository: IOrderRepository = new OrderRepository();
+export const orderRepository: IOrderRepository = new OrderRepository(prisma);
