41e570a9b0c9776d428203f973db9015006948d3
trantuananh-17 <anhkyohauik17@gmail.com>
2025-09-29 13:04:34 +0700

    refactor: di auth

--- Backend/src/modules/user/Repository/implements/user.repository.impl.ts
+++ Backend/src/modules/user/Repository/implements/user.repository.impl.ts
@@ -1,13 +1,15 @@
 import { User } from '@prisma/client';
 import prisma from '~/prisma';
 import { IUserRepository } from '../user.repository';
+import { PrismaClient } from '@prisma/client/extension';
 
 class UserRepository implements IUserRepository {
+  constructor(private readonly prisma: PrismaClient) {}
   public async findUserByEmail(email: string): Promise<User | null> {
-    return await prisma.user.findFirst({
+    return await this.prisma.user.findFirst({
       where: { email }
     });
   }
 }
 
-export const userRepository: IUserRepository = new UserRepository();
+export const userRepository: IUserRepository = new UserRepository(prisma);
