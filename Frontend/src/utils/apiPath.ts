7e93f7fd8a4612c8b92b93d23bbcb42f3c36e18d
trantuananh-17 <anhkyohauik17@gmail.com>
2025-10-04 12:21:12 +0700

    add

---
+++ Frontend/src/utils/apiPath.ts
@@ -0,0 +1,9 @@
+export const BASE_URL = 'http://localhost:5000';
+
+export const API_PATHS = {
+  AUTH: {
+    REGISTER: '/api/auth/register',
+    LOGIN: '/api/auth/login',
+    GET_PROFILE: '/api/user/profile'
+  }
+};
